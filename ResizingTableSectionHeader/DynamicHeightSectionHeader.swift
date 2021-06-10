8a25ea6c43b1c42074fa84744b111a5cba07dbcd
Brad Leege <bleege@gmail.com>
2021-06-09 22:16:25 -0500

    Start Header In collapsed state

--- ResizingTableSectionHeader/DynamicHeightSectionHeader.swift
+++ ResizingTableSectionHeader/DynamicHeightSectionHeader.swift
@@ -20,11 +20,14 @@ class DynamicHeightSectionHeader: UIViewController {
 
         // Do any additional setup after loading the view.
         print("viewDidLoad's headerHeightContstraint constant = \(headerHeightConstraint.constant)")
+        self.headerHeightConstraint.constant = collapseHeight
+        print("viewDidLoad's updated headerHeightContstraint constant = \(headerHeightConstraint.constant)")
         
         let width = UIScreen.main.bounds.width
         print("Screen width = \(width); Stock headerWidthConstraint = \(headerWidthConstraint.constant)")
         headerWidthConstraint.constant = width
         print("New headerWidthConstraint = \(headerWidthConstraint.constant)")
+        self.view.layoutIfNeeded()
     }
 
 }
