bd14bf28d16dbfce19f266fb413604c0e58d801d
Brad Leege <bleege@gmail.com>
2021-06-09 20:29:31 -0500

    Initial Commit

---
+++ ResizingTableSectionHeader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResizingTableSectionHeader
+//
+//  Created by Brad Leege on 6/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
